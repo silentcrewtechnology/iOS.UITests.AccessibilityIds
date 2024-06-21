67ac92c396a0bd4d9fa87c40ecd33218b879b0ec
Омельченко Юлия Константиновна <omelchenkoyuk@akbarsdigital.ru>
2024-06-21 17:19:03 +0300

    PCABO3-10884: Добавила accessibility ids для конвеера валют

---
+++ Sources/AccessibilityIds/CurrenciesAccessibilityIDs/CurrenciesAccessibilityIDs.swift
@@ -0,0 +1,22 @@
+//
+//  CurrenciesAccessibilityIDs.swift
+//
+//
+//  Created by Омельченко Юлия on 21.06.2024.
+//
+
+import Foundation
+
+open class CurrenciesAccessibilityIDs {
+    /// Главный экран
+    open class QuotationListScreen {
+        public static let id = "quotation_list_table_view"
+        public static let activityIndicator = "quotation_list_activity_indicator"
+        
+        public static let headerInfoRow = "header_info_row"
+        public static let headerInfoRowLabel = "header_info_row_label"
+        
+        public static let infoTextRow = "info_text_row"
+        public static let infoTextRowLabel = "info_text_row_label"
+    }
+}
