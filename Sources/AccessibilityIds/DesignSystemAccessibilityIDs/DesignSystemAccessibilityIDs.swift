77855104da0a74d931b8a7d316c640632f8481d4
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-06-24 13:22:15 +0300

    PCABO3-10861: Добавил id для LabelView

---
+++ Sources/AccessibilityIds/DesignSystemAccessibilityIDs/DesignSystemAccessibilityIDs.swift
@@ -0,0 +1,15 @@
+//
+//  DesignSystemAccessibilityIDs.swift
+//
+//
+//  Created by user on 24.06.2024.
+//
+
+import Foundation
+
+open class DesignSystemAccessibilityIDs {
+    open class LabelView {
+        public static let id = "label_view"
+        public static let labelView = "label_view_label_view"
+    }
+}
