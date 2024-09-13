0ccebc308a322d0302b13a0f559d71d0093cc6fc
Омельченко Юлия Константиновна <omelchenkoyuk@abdgtl.com>
2024-09-13 11:07:35 +0300

    PCABO3-11186: Добавила ids для InputView и ButtonView

--- Sources/AccessibilityIds/DesignSystemAccessibilityIDs/DesignSystemAccessibilityIDs.swift
+++ Sources/AccessibilityIds/DesignSystemAccessibilityIDs/DesignSystemAccessibilityIDs.swift
@@ -9,6 +9,7 @@ public class DesignSystemAccessibilityIDs {
     public class LabelView {
         public static let id = "label_view"
         public static let labelView = "label_view_label_view"
+        public static let container = "label_view_container"
     }
     
     public class SegmentItemView {
@@ -21,5 +22,19 @@ public class DesignSystemAccessibilityIDs {
         public static let id = "segment_control_view"
         public static let containerView = "segment_control_container_view"
         public static let segmentStack = "segment_control_stack"
+    }
+    
+    public class InputView {
+        public static let id = "input_view"
+        public static let label = "input_view_label"
+        public static let hint = "input_view_hint"
+        public static let textField = "input_view_text_field"
+        public static let verticalStack = "input_view_stack"
+        public static let textFieldContainer = "input_view_text_field_container"
+    }
+    
+    public class ButtonView {
+        public static let id = "button_view"
+        public static let label = "button_label"
     }
 }
