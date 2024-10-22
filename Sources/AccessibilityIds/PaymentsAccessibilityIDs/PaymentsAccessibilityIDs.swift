3ff2f33a67a98e002de5d2967c6865c68026dc92
Омельченко Юлия Константиновна <omelchenkoyuk@abdgtl.com>
2024-10-22 10:27:26 +0300

    PCABO3-11883: Добавила ids для таб-бара и экрана истории

--- Sources/AccessibilityIds/PaymentsAccessibilityIDs/PaymentsAccessibilityIDs.swift
+++ Sources/AccessibilityIds/PaymentsAccessibilityIDs/PaymentsAccessibilityIDs.swift
@@ -10,4 +10,17 @@ public class PaymentsAccessibilityIDs {
     public class PaymentsMainScreen {
         public static let id = "payments_main_table_view"
     }
+    
+    /// Экран истории операций
+    public class HistoryScreen {
+        public static let id = "history_screen_view"
+    }
+    
+    /// Таб-бар
+    public class TabBar {
+        public static let id = "tab_bar"
+        public static let mainItem = "tab_bar_item_main"
+        public static let paymentsItem = "tab_bar_item_payments"
+        public static let historyItem = "tab_bar_item_history"
+    }
 }
